caa69bedf66f64a8fba5615f8adbfa5b44e94746
Joseph Cheek <joseph@cheek.com.co>
2015-10-01 19:46:01 -0400

    make bg images 1600px wide max

--- sites/all/themes/gdg/template.php
+++ sites/all/themes/gdg/template.php
@@ -10,7 +10,7 @@ function gdg_preprocess_html(&$variables) {
 
     if (isset($node->field_background_image[LANGUAGE_NONE][0]['uri'])) {
 
-      $image_uri = file_create_url(
+      $image_uri = image_style_url('1600px_wide',
         $node->field_background_image[LANGUAGE_NONE][0]['uri']
       );
 
