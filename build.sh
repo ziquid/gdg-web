03535e991161296d0f4119195bd1d10482c44960
Joseph Cheek <joseph@gdgwebworks.com>
2016-07-29 18:25:26 -0400

    misc updates, alpha-blended logo

--- build.sh
+++ build.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-DRUSH=`which d7`
-[ "$DRUSH" == "" ] && DRUSH=`which d6`
-[ "$DRUSH" == "" ] && DRUSH=`which drush`
+DRUSH=`which d7 2>/dev/null`
+[ "$DRUSH" == "" ] && DRUSH=`which d6 2>/dev/null`
+[ "$DRUSH" == "" ] && DRUSH=`which drush 2>/dev/null`
 
 SCOPE=local
-[ "$PWD" == "/var/www/dev.fitnessne.ws/htdocs" ] && SCOPE=dev
-[ "$PWD" == "/var/www/fitnessne.ws/htdocs" ] && SCOPE=prod
+echo "$PWD" | grep -s -q htdocs && SCOPE=prod
+echo "$PWD" | grep -s -q dev && SCOPE=dev
 
+chmod ug+w sites/default
+cp .gitignore gitignore
 $DRUSH -y -q make gdg.make
+mv gitignore .gitignore
 $DRUSH -y en master features
-# $DRUSH -y master-execute --scope=$SCOPE
+echo enabling modules with a scope of $SCOPE
+$DRUSH -y master-execute --scope=$SCOPE
 $DRUSH -y fra
 $DRUSH -y updb
-$DRUSH cc all
+# $DRUSH cc all
